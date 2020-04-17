5c579c462f3f988e8769e94d4756269da661fe8a
Eriberto Guzman <eriberto176@gmail.com>
2020-04-17 13:24:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -144,14 +144,16 @@ function moveDodger(e) {
    
    
    if (e.which === LEFT_ARROW){
-     moveDodgerLeft();
+     
      e.preventDefault();
      e.stopPropagation();
+     moveDodgerLeft();
    }
    else if(e.which === RIGHT_ARROW){
-     moveDodgerRight();
+     
      e.preventDefault();
      e.stopPropagation();
+     moveDodgerRight();
    }
    
 }
