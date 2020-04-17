f915511cf62844f0c499cb0862ac306e474eebc4
Eriberto Guzman <eriberto176@gmail.com>
2020-04-17 13:37:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -89,7 +89,7 @@ function createRock(x) {
      * we should remove the rock from the DOM.
      */
      
-     rock.style.top = `${top+=1}px`;
+     rock.style.top = `${top+=4}px`;
      if (checkCollision(rock)){
       endGame()
        
